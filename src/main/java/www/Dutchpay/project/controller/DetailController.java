17fe7f6d21b5b2f85de0377643a15d4c57aedf61
박현수 <pm6567@naver.com>
2024-12-04 21:11:32 +0900

    refatcor : controller 수정

--- src/main/java/www/Dutchpay/project/controller/DetailController.java
+++ src/main/java/www/Dutchpay/project/controller/DetailController.java
@@ -1,14 +1,10 @@
 package www.Dutchpay.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import www.Dutchpay.project.dto.DetailsDTO;
 import www.Dutchpay.project.service.DetailsService;
 
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -19,7 +15,6 @@ public class DetailController {
     DetailsService detailsService;
     @PostMapping("/get")
     public List<DetailsDTO> getList(@RequestBody DetailsDTO detailsDTO){
-        System.out.println(detailsDTO.getGroup_id()+"컨트롤러");
         return detailsService.getList(detailsDTO.getGroup_id());
     }
 }
