461436a55307de25cf2af5d6cb78bf2e1342172c
ByeonKwon <cbk5126@gmail.com>
2024-12-04 16:18:47 +0900

    dto 생성

---
+++ src/main/java/www/Dutchpay/project/dto/GroupDto.java
@@ -0,0 +1,11 @@
+package www.Dutchpay.project.dto;
+
+import lombok.Data;
+
+@Data
+public class GroupDto {
+
+    private int group_id;
+    private String group_name;
+    private int device_id;
+}
