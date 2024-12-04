c774178622b327d077f84037689d36a1cb60087e
ByeonKwon <cbk5126@gmail.com>
2024-12-04 16:18:20 +0900

    컨트롤러 그룹 가져오기 기능 ㅊ추가

---
+++ src/main/java/www/Dutchpay/project/controller/GroupController.java
@@ -0,0 +1,25 @@
+package www.Dutchpay.project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import www.Dutchpay.project.dto.GroupDto;
+import www.Dutchpay.project.service.GroupService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Group")
+public class GroupController {
+
+    @Autowired
+    GroupService groupService;
+
+    @GetMapping("/getGroup")
+    public List<GroupDto> Collect() {
+        return groupService.getGroup();
+    }
+
+
+
+
+}
