69042b9ff952366eadfdc185695947436e6faa1b
ByeonKwon <cbk5126@gmail.com>
2024-12-04 16:19:25 +0900

    mapper에 그룹 가져오기 기능 추가

---
+++ src/main/java/www/Dutchpay/project/mapper/GroupMapper.java
@@ -0,0 +1,16 @@
+package www.Dutchpay.project.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import www.Dutchpay.project.dto.GroupDto;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface GroupMapper {
+
+    List<GroupDto> getGroups();
+
+}
