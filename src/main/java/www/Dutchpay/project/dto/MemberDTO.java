851b65f434ca5405dfd5087526f91e6fb4d074d4
박현수 <pm6567@naver.com>
2024-12-05 16:43:11 +0900

    feat : Member DTO

---
+++ src/main/java/www/Dutchpay/project/dto/MemberDTO.java
@@ -0,0 +1,13 @@
+package www.Dutchpay.project.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberDTO {
+
+    private int device_id;
+    private int group_id;
+    private int list_id;
+    private int member_id;
+    private String member;
+}
