cd0d8084e7c4439e81ef83b84802c096ecf8496d
박현수 <pm6567@naver.com>
2024-12-05 16:44:07 +0900

    feat : member) Service : group_id 통한 맴버 가져오기

---
+++ src/main/java/www/Dutchpay/project/service/MemberService.java
@@ -0,0 +1,20 @@
+package www.Dutchpay.project.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import www.Dutchpay.project.dto.MemberDTO;
+import www.Dutchpay.project.mapper.MemberMapper;
+
+import java.util.List;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    MemberMapper memberMapper;
+    /*Group에 관한 맴버를 가져옴 group_id 통해서 가져옴*/
+    public List<MemberDTO> GroupMember (MemberDTO memberDTO) {
+
+        return memberMapper.GroupMember(memberDTO.getGroup_id());
+    }
+}
