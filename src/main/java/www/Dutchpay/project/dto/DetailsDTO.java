bf49a312b3558d0b47180e3af60221fbcd6994c3
박현수 <pm6567@naver.com>
2024-12-05 11:32:16 +0900

    refactor : DTO List_id -> list_id

--- src/main/java/www/Dutchpay/project/dto/DetailsDTO.java
+++ src/main/java/www/Dutchpay/project/dto/DetailsDTO.java
@@ -7,7 +7,7 @@ public class DetailsDTO {
 
     private int device_id;
     private int group_id;
-    private int List_id;
+    private int list_id;
 
     private String title;
     private int money;
