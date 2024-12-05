07877bd175099d8ece071a17f60f2272db0cf306
박현수 <pm6567@naver.com>
2024-12-05 09:05:44 +0900

    refatcor : ResponseEntity 담아서 return 수정

--- src/main/java/www/Dutchpay/project/controller/DetailController.java
+++ src/main/java/www/Dutchpay/project/controller/DetailController.java
@@ -1,6 +1,7 @@
 package www.Dutchpay.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import www.Dutchpay.project.dto.DetailsDTO;
 import www.Dutchpay.project.service.DetailsService;
@@ -14,7 +15,7 @@ public class DetailController {
     @Autowired
     DetailsService detailsService;
     @PostMapping("/get")
-    public List<DetailsDTO> getList(@RequestBody DetailsDTO detailsDTO){
-        return detailsService.getList(detailsDTO.getGroup_id());
+    public ResponseEntity<List<DetailsDTO>> getList(@RequestBody DetailsDTO detailsDTO){
+        return ResponseEntity.ok(detailsService.getList(detailsDTO.getGroup_id()));
     }
 }
