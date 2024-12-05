4a84af38534901ae6bce4fb6f29c22182936fc36
박현수 <pm6567@naver.com>
2024-12-05 14:46:13 +0900

    refactor : details) delete, update 칸 개수를 반환받아서 true false로 넘겨줌

--- src/main/java/www/Dutchpay/project/service/DetailsService.java
+++ src/main/java/www/Dutchpay/project/service/DetailsService.java
@@ -24,23 +24,22 @@ public class DetailsService {
         return "성공";
     }
 
-    /* Delete 로직인데 try catch문 예외에 걸리지않고 다  true 반환*/
-
+    /* Delete 로직 반환되는 cnt이 0이면 없어진게없으니 false 있으면 true */
     public boolean deleteList(DetailsDTO detailsDTO){
-        try {
-            detailsMapper.deleteDetails(detailsDTO.getDevice_id(),detailsDTO.getGroup_id(),detailsDTO.getTitle());
-            return true;
-        }catch (Exception e){
-            return false;
-        }
-    }
 
+        int rowcnt =detailsMapper.deleteDetails(detailsDTO.getDevice_id(),detailsDTO.getGroup_id(),detailsDTO.getTitle());
+         if(rowcnt == 0){
+             return false;
+         }
+            return true;
+    }
+/* Update 로직 반환되는 row수가 있으면 0보다큼 아니면 false */
     public boolean updateList(DetailsDTO detailsDTO){
-       try{
-           detailsMapper.updateDetails(detailsDTO.getList_id(),detailsDTO.getTitle(),detailsDTO.getMoney());
+          int rowcnt =  detailsMapper.updateDetails(detailsDTO.getList_id(),detailsDTO.getTitle(),detailsDTO.getMoney());
+          if(rowcnt ==0){
+              return false;
+          }
            return true;
-       }catch ( Exception e){
-           return false;
-       }
+
     }
 }
