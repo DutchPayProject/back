8c7624affb127791097b4676843933a337157d14
ByeonKwon <cbk5126@gmail.com>
2024-12-05 16:29:07 +0900

    Mapper에 deleteGroup int로 자료형 변경 및 매겨변수 변경

--- src/main/java/www/Dutchpay/project/mapper/GroupMapper.java
+++ src/main/java/www/Dutchpay/project/mapper/GroupMapper.java
@@ -12,6 +12,6 @@ public interface GroupMapper {
 
     List<GroupDto> getGroups();
     void insertGroup(GroupDto group);
-    void deleteGroup(GroupDto group);
+    int deleteGroup(int group_id, String group_name,int device_id);
     int updateGroup(int group_id, int device_id, String group_name);
 }
