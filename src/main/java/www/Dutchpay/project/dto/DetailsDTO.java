714ab5e81122b16129b58de89d782d1ebc57174f
박현수 <pm6567@naver.com>
2024-12-12 09:34:33 +0900

    refactor : application properties : aliases 경로 수정,DTO Lombok수정

--- src/main/java/www/Dutchpay/project/dto/DetailsDTO.java
+++ src/main/java/www/Dutchpay/project/dto/DetailsDTO.java
@@ -3,8 +3,10 @@ package www.Dutchpay.project.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
 @Data
+@Log4j
 @NoArgsConstructor
 @AllArgsConstructor
 public class DetailsDTO {
